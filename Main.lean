faed3d69cf8e61484cbc08aa736cb3ba6cf1c6d4
AntanasKal <a.kalkauskas@gmail.com>
2023-02-11 21:25:28 +0200

    using multiple threads for rendering, adding README and example result image

--- Main.lean
+++ Main.lean
@@ -10,12 +10,12 @@ def pi : Float := 3.1415926535897932385
 
 
 def aspect_ratio : Float := 3.0/2.0
-def image_width : Nat := 500
+def image_width : Nat := 1200
 def image_height : Nat := (image_width.toFloat / aspect_ratio).toUInt32.toNat
 def max_range : Float := 255.999
 
-def samples_per_pixel := 50
-def max_depth := 20
+def samples_per_pixel := 240
+def max_depth := 30
 
 
 def lookfrom : point3 := (point3.mk (vec3.mk (13) 2 3))
@@ -79,18 +79,30 @@ def get_colour_sum_io (world : List hittable) (col_sum : colour) (i j : Nat) (sa
       pure (← get_colour_sum_io world (col_sum.add r_col) i j nn depth)
 
 
-def print_gradient (world : List hittable) (i : Nat) (j : Nat) : IO Unit :=
+def print_gradient (world : List hittable) (samples : Nat) (i j: Nat) : IO Unit :=
   do 
-    let col_sum ←  get_colour_sum_io world (colour.mk 0 0 0) i j samples_per_pixel max_depth
+    let col_sum ←  get_colour_sum_io world (colour.mk 0 0 0) i j samples max_depth
     col_sum.write
 
 
 def print_picture_line (world : List hittable) (i j : Nat) : IO Unit :=
   do
-    print_gradient world (image_width - i - 1) j
+    print_gradient world samples_per_pixel (image_width - i - 1) j
     match i with
     | 0 => pure ()
     | ii+1 => print_picture_line world ii j
+
+def get_picture_line (world : List hittable) (samples : Nat) (i j : Nat) : IO (List colour) := do
+  let col_sum ← get_colour_sum_io world (colour.mk 0 0 0) (image_width - i - 1) j samples max_depth;
+  match i with
+  | 0 => pure [col_sum]
+  | ii+1 => pure $ col_sum :: (← get_picture_line world samples ii j)
+
+def print_line (col_list : List colour) : IO Unit :=
+  match col_list with
+  | [] => pure ()
+  | (x::xs) => do x.write; print_line xs
+
 
 def print_picture (world : List hittable) (i j : Nat) : IO Unit :=
   do
@@ -99,8 +111,46 @@ def print_picture (world : List hittable) (i j : Nat) : IO Unit :=
     match j with
     | 0 => pure ()
     | jj+1 => print_picture world (image_width-1) jj
+
+
+def sum_colour_lists (list_of_lists : List (List colour)) : (List colour) :=
+  match list_of_lists with
+  | [] => []
+  | [ls1] => ls1
+  | (ls1::ls2::[]) => (List.zipWith colour.add ls1 ls2)
+  | (ls1::ls2::lss) => List.zipWith colour.add (List.zipWith colour.add ls1 ls2) (sum_colour_lists lss)
+
+
+def print_picture_multithreaded (world : List hittable) (number_of_threads : Nat) (width row : Nat) : IO Unit :=
+  do
+    (← IO.getStderr).putStrLn s!"\r Scanlines remaining: {row}"
+    let mut taskList : List ( Task (Except IO.Error (List colour)) ):= []
+    for _ in [0:number_of_threads]
+      do 
+        let a_task ←  (get_picture_line world (samples_per_pixel/number_of_threads) width row).asTask
+        taskList := (a_task:: taskList)
     
-  
+    let mut line_list : List (List colour) := []
+    for i in [0:number_of_threads]
+      do
+        let tsk? := List.get? taskList i
+        match tsk? with
+        | some tsk => 
+            let task_result := tsk.get
+            match task_result with
+            | Except.error a => pure ()
+            | Except.ok result =>
+                line_list := (result :: line_list)
+        | none     => pure ()
+
+    let line := sum_colour_lists line_list
+    print_line line
+    match row with
+    | 0 => pure ()
+    | jj+1 => print_picture_multithreaded world number_of_threads width jj
+
+def num_threads := 6
+
 def main : IO Unit := do
   let stdout ← IO.getStdout;
   stdout.putStrLn "P3";
@@ -108,4 +158,15 @@ def main : IO Unit := do
   stdout.putStrLn s!"255"
   let world ← generate_world
   (←  IO.getStderr).putStrLn s!"{world.length}"
-  print_picture world (image_width-1) (image_height-1)
+  print_picture_multithreaded world num_threads (image_width-1) (image_height-1)
+
+
+
+-- def main : IO Unit := do
+--   let stdout ← IO.getStdout;
+--   stdout.putStrLn "P3";
+--   stdout.putStrLn s!"{image_width} {image_height}";
+--   stdout.putStrLn s!"255"
+--   let world ← generate_world
+--   (←  IO.getStderr).putStrLn s!"{world.length}"
+--   print_picture world (image_width-1) (image_height-1)
---
+++ README.md
@@ -0,0 +1,8 @@
+# Simple Lean4 Raytracing
+
+My take of implementing a toy raytracer in Lean4 following [Ray Tracing in One Weekend](https://raytracing.github.io/books/RayTracingInOneWeekend.html) book. I am not the first one to implement such raytracer in Lean4, this has also been done [here](https://github.com/kmill/lean4-raytracer).
+
+## Pictures
+
+
+![One Weekend cover recreated, 69m11,019s on 6 threads](./imgs/tutorial-depth-30-samples-240.ppm)
